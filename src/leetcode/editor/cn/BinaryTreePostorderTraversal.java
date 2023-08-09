667b513032bdb77f5d1aa17b906bac5d6a26f342
Leon <leon@msi-mac.local>
2023-08-09 23:23:49 +0800

    feat: init project

---
+++ src/leetcode/editor/cn/BinaryTreePostorderTraversal.java
@@ -0,0 +1,75 @@
+package leetcode.editor.cn;
+
+import leetcode.editor.cn.base.TreeNode;
+
+import java.util.List;
+
+/**
+<p>给你一棵二叉树的根节点 <code>root</code> ，返回其节点值的 <strong>后序遍历 </strong>。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2020/08/28/pre1.jpg" style="width: 127px; height: 200px;" /> 
+<pre>
+<strong>输入：</strong>root = [1,null,2,3]
+<strong>输出：</strong>[3,2,1]
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<strong>输入：</strong>root = []
+<strong>输出：</strong>[]
+</pre>
+
+<p><strong>示例 3：</strong></p>
+
+<pre>
+<strong>输入：</strong>root = [1]
+<strong>输出：</strong>[1]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul> 
+ <li>树中节点的数目在范围 <code>[0, 100]</code> 内</li> 
+ <li><code>-100 &lt;= Node.val &lt;= 100</code></li> 
+</ul>
+
+<p>&nbsp;</p>
+
+<p><strong>进阶：</strong>递归算法很简单，你可以通过迭代算法完成吗？</p>
+
+<div><div>Related Topics</div><div><li>栈</li><li>树</li><li>深度优先搜索</li><li>二叉树</li></div></div><br><div><li>👍 1069</li><li>👎 0</li></div>
+*/
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+    }
+    
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
